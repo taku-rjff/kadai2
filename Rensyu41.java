6431c2d152420aa0b30283177b64b70e9d2f331c
asct04 <asct04@T-PC04.mshome.net>
2023-09-05 16:07:12 +0900

    更新

--- Rensyu41.java
+++ Rensyu41.java
@@ -5,7 +5,7 @@ public class Rensyu41 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n, k;
-		System.out.print("1以上の整数:");
+		System.out.print("0以上の整数:");
 		n = sc.nextInt();
 		System.out.print("引数:");
 		k = sc.nextInt();
